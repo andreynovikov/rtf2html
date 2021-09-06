d84f26e9c18db3cb4a97e598b7be4bab07401cc6
Andrey Novikov <novikov@gmail.com>
2021-09-06 17:59:22 +0300

    Update document.py

--- rtf2html/document.py
+++ rtf2html/document.py
@@ -1,1 +1,36 @@
+class _BaseElement(object):
+    def __init__(self, properties={}, content=[]):
+        self.properties = {}
+        self.content = []
+
+        for (k, v) in properties.items():
+            self[k] = v
+
+        for item in content:
+            self.append(item)
+
+    def __setitem__(self, key, value):
+        self.properties[key] = value
+
+    def __getitem__(self, key):
+        return self.properties.get(key)
+
+    def append(self, item):
+        self.content.append(item)
+
+
+class Container(_BaseElement):
+    pass
+
+
+class Text(_BaseElement):
+    pass
+
+
+class Image(_BaseElement):
+    pass
+
+
+class Html(Text):
+    pass
 
